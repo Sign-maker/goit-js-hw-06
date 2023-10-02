4bf859e6974c16c00df76fbc8908504fec43f89f
Sign-maker <muratovdv76@gmail.com>
2023-10-02 11:35:48 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,23 +1,17 @@
-const inputBox = document.querySelector("#validation-input");
+const inputBox = document.querySelector('#validation-input');
 
-inputBox.addEventListener("blur", onBlur);
+inputBox.addEventListener('blur', onBlur);
 
 function onBlur(event) {
-  //   console.log(event);
-  const inputClassList = inputBox.classList;
-  const valueLength = event.currentTarget.value.length;
-  const dataLength = event.currentTarget.dataset.length;
+	const valueLength = event.currentTarget.value.length;
+	const dataLength = Number(event.currentTarget.dataset.length);
 
-  if (valueLength < dataLength) {
-    inputClassList.contains("valid")
-      ? inputClassList.replace("valid", "invalid")
-      : inputBox.classList.add("invalid");
-  } else
-    inputClassList.contains("invalid")
-      ? inputClassList.replace("invalid", "valid")
-      : inputBox.classList.add("valid");
+	// console.log(valueLength, typeof valueLength);
+	// console.log(dataLength, typeof dataLength);
+	// console.log(valueLength !== dataLength);
 
-  // console.log(inputBox);
+	inputBox.classList.toggle('invalid', valueLength !== dataLength);
+	inputBox.classList.toggle('valid', valueLength == dataLength);
 }
 
 // Напиши скрипт, який під час втрати фокусу на інпуті(подія blur), перевіряє його вміст щодо правильної кількості введених символів.
@@ -26,19 +20,35 @@ function onBlur(event) {
 
 // Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
 
-// Второй вариант, не такой красивый, но нет лишнего действия add, когда выполняются условия 3 и 4, хотя add под капотом, и само проверяет, есть ли такой класс
-// 1. value.length < dataset.length && !inputClassList.contains("invalid")&&!inputClassList.contains("valid")
-// inputBox.classList.add("invalid");
+// if (valueLength !== dataLength) {
+// 	inputClassList.contains('valid')
+// 		? inputClassList.replace('valid', 'invalid')
+// 		: inputBox.classList.add('invalid');
+// } else
+// 	inputClassList.contains('invalid')
+// 		? inputClassList.replace('invalid', 'valid')
+// 		: inputBox.classList.add('valid');
+// ==============================================
+// if (
+// 	valueLength !== dataLength &&
+// 	!inputClassList.contains('valid') &&
+// 	!inputClassList.contains('invalid')
+// ) {
+// 	inputBox.classList.add('invalid');
+// }
 
-// 2. value.length > dataset.length && !inputClassList.contains("invalid")&&!inputClassList.contains("valid")
-// inputBox.classList.add("valid");
+// if (
+// 	valueLength == dataLength &&
+// 	!inputClassList.contains('valid') &&
+// 	!inputClassList.contains('invalid')
+// ) {
+// 	inputBox.classList.add('valid');
+// }
 
-// 5. value.length < dataset.length && inputClassList.contains("valid")
-// inputClassList.replace("valid", "invalid")
-// 6. value.length > dataset.length && inputClassList.contains("invalid")
-//inputClassList.replace("invalid", "valid")
+// if (valueLength !== dataLength && inputClassList.contains('valid')) {
+// 	inputClassList.replace('valid', 'invalid');
+// }
 
-// 3. value.length < dataset.length && inputClassList.contains("invalid")
-// no action
-// 4. value.length > dataset.length && inputClassList.contains("valid")
-// no action
+// if (valueLength == dataLength && inputClassList.contains('invalid')) {
+// 	inputClassList.replace('invalid', 'valid');
+// }
